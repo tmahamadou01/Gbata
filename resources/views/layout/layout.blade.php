df3c191d5911c6087bb7d0bacf052e18f33a6e89
tmahamadou01 <mahamadoutraore1@gmail.com>
2017-01-09 23:26:41 +0100

    update background color

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -36,7 +36,7 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body>
+<body style="background-color: #f2f2f2;">
     <div id="menu-bar">
         <div class="container">
             <div class="col-xs-2">
